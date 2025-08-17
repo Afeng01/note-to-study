2c3f31f99d569e9209b6fd1bb11783d9f7c939da
Afeng01 <53217394+Afeng01@users.noreply.github.com>
2025-08-17 11:24:25 +0800

    初步配置。

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
   description: "筱言浅语",
   lang: 'zh-CN', // 语言
   base: '/note-to-study/', // ⬅️ 非常重要！部署到 GitHub Pages 时的基础路
-  
+
   //主题配置
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
@@ -18,24 +18,15 @@ export default defineConfig({
 
     //侧边栏
     sidebar: [
-      // 这是一个例子，路径要和你 docs 文件夹下的文件结构对应
-      '/text/' [
-        {
-          text: '文档',
-          collapsed: false, // 默认是否折叠
-          items: [
-            { text: '1.1开始', link: '/text/chapter1/1-start' },
-            { text: 'Runtime API Examples', link: '/api-examples' }
-          ]
-        },
-        {
-          text: '第二章：使用数据构造抽象',
-          collapsed: true,
-          items: [
-            // ...
-          ]
-        }
-      ]
+      { text: '开始', link: '/text/index' },
+      {
+        text: '在进行的内容',
+        collapsed: false, // 默认是否折叠
+        items: [
+          { text: '1.1 开始', link: '/text/chapter1/1-start' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
     ],
 
     // 右上角的社交链接
